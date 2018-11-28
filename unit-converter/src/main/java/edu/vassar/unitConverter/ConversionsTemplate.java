0d31cf6261462614b127d2889ee19e9f2e95954f
analogclock <greer.hoffmann@gmail.com>
2018-11-27 22:00:52 -0500

    Added Javadoc comments to interface

--- unit-converter/src/main/java/edu/vassar/unitConverter/ConversionsTemplate.java
+++ unit-converter/src/main/java/edu/vassar/unitConverter/ConversionsTemplate.java
@@ -7,13 +7,52 @@ package edu.vassar.unitConverter;
 
 /**
  *
- * @author greerhoffmann
+ * @author Middle-Earth (Middle-Left group)
  */
 public interface ConversionsTemplate {
-    
-    public double feetToMeters(double feet);
-    public double metersToFeet(double feet);
-    public double pintsToLiters(double pints);
-    public double litersToPints(double liters);
-    
+
+    /**
+     * CONVERTDISTANCE
+     *
+     * @param unitAndValSource, a string in the format "unit_value", e.g.
+     * "feet_10.9"
+     * @param targetUnit, a string containing unit to convert to, e.g. "meters"
+     * @return a string containing the target unit and converted value, e.g.
+     * "meters_3.32232"
+     */
+    public String convertDistance(String unitAndValSource, String targetUnit);
+
+    /**
+     * CONVERTAREAS
+     * @param unitAndValSource, a string in the format "unit_value",
+     *  e.g. "feet_10.9"
+     * @param targetUnit, a string containing unit to convert to,
+     *  e.g. "meters"
+     * @return a string containing the target unit and converted value,
+     *  e.g. "meters_3.32232"
+     */
+    public String convertAreas(String unitAndValSource, String targetUnit);
+
+    /**
+     * CONVERTVOLUME
+     * @param unitAndValSource, a string in the format "unit_value",
+     *  e.g. "feet_10.9"
+     * @param targetUnit, a string containing unit to convert to,
+     *  e.g. "meters"
+     * @return a string containing the target unit and converted value,
+     *  e.g. "meters_3.32232"
+     */
+    public String convertVolume(String unitAndValSource, String targetUnit);
+
+    /**
+     * CONVERTTEMPERATURES
+     * @param unitAndValSource, a string in the format "unit_value",
+     *  e.g. "feet_10.9"
+     * @param targetUnit, a string containing unit to convert to,
+     *  e.g. "meters"
+     * @return a string containing the target unit and converted value,
+     *  e.g. "meters_3.32232"
+     */
+    public String convertTemperatures(String unitAndValSource, String targetUnit);
+
 }

0ceb0cff374f1903f94513552f897e94b2c949ba
analogclock <greer.hoffmann@gmail.com>
2018-12-04 16:32:38 -0500

    Added an interface PrintResult that prints the result of the conversion

---
+++ unit-converter/src/main/java/edu/vassar/unitConverter/PrintResult.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.vassar.unitConverter;
+
+/**
+ *
+ * @author greerhoffmann
+ */
+public interface PrintResult {
+    
+    public String printResult(double result);
+    
+}

afd04508671eeda2b9925f84a2207a608bc871bc
MadisonStiefel <madison55stiefel@gmail.com>
2018-12-04 16:58:12 -0500

    Reformated method names

--- unit-converter/src/main/java/edu/vassar/unitConverter/TemperatureConverter.java
+++ unit-converter/src/main/java/edu/vassar/unitConverter/TemperatureConverter.java
@@ -8,37 +8,37 @@ public class TemperatureConverter {
   double inputValue;
   double outputValue;
 
-  public void name() {
-
+  public double selectUnitsOfConversion(String inputUnit, String outputUnit) {
+    return 0.0;
   }
 
   // F = C * (9/5) + 32
-  public double CelsiusToFahrenheit(double input) {
+  public double celsiusToFahrenheit(double input) {
     return 0.0;
   }
 
   // C = (F - 32) * (9/5)
-  public double FahrenheitToCelsius(double input) {
+  public double fahrenheitToCelsius(double input) {
     return 0.0;
   }
 
   // K = C + 273.15
-  public double CelsiusToKelvin(double input) {
+  public double celsiusToKelvin(double input) {
     return 0.0;
   }
 
   // C = K - 273.15
-  public double KelvinToCelsius(double input) {
+  public double kelvinToCelsius(double input) {
     return 0.0;
   }
 
   // K = (F + 459.67) * (5/9)
-  public double FahrenheitToKelvin(double input) {
+  public double fahrenheitToKelvin(double input) {
     return 0.0;
   }
 
   // F = K * (9/5) - 459.67
-  public double KelvinToFahrenheit(double input) {
+  public double kelvinToFahrenheit(double input) {
     return 0.0;
   }
 

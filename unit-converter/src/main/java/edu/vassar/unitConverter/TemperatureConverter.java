d99aa412183fa5878f7fa1e33ec486f55316a368
MadisonStiefel <madison55stiefel@gmail.com>
2018-12-04 16:50:24 -0500

    Added more units of conversion

--- unit-converter/src/main/java/edu/vassar/unitConverter/TemperatureConverter.java
+++ unit-converter/src/main/java/edu/vassar/unitConverter/TemperatureConverter.java
@@ -1,6 +1,7 @@
 public class TemperatureConverter {
-  // Kelvin
+  // Celsisus
   // Fahrenheit
+  // Kelvin
 
   String inputUnit;
   String outputUnit;
@@ -18,6 +19,26 @@ public class TemperatureConverter {
 
   // C = (F - 32) * (9/5)
   public double FahrenheitToCelsius(double input) {
+    return 0.0;
+  }
+
+  // K = C + 273.15
+  public double CelsiusToKelvin(double input) {
+    return 0.0;
+  }
+
+  // C = K - 273.15
+  public double KelvinToCelsius(double input) {
+    return 0.0;
+  }
+
+  // K = (F + 459.67) * (5/9)
+  public double FahrenheitToKelvin(double input) {
+    return 0.0;
+  }
+
+  // F = K * (9/5) - 459.67
+  public double KelvinToFahrenheit(double input) {
     return 0.0;
   }
 
